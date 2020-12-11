fac38997e239cbdcd3ee6b60a723c5505d146467
AkidSe <akidse1@gmail.com>
2020-12-12 01:11:45 +0200

    fixed env variables

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,7 @@
+console.log(process.env);
 export const API_CONST = {
-    url: process.env.API_SERVER_LINK || '',
-    sockets_url: process.env.SOCKET_SERVER_LINK || '',
+    url: process.env.REACT_APP_API_SERVER_LINK || '',
+    sockets_url: process.env.REACT_APP_SOCKET_SERVER_LINK || '',
 };
+
+console.log(API_CONST);
